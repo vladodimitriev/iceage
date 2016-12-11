648a85525f63bd9ac31284d1e8321445ff4d37b8
gocedelcev <delcevmkd@gmail.com>
2016-12-11 16:28:09 +0100

    Add fragments

---
+++ app/src/main/java/cf/vlado/iceage/app/IceDetailFragment.java
@@ -0,0 +1,11 @@
+package cf.vlado.iceage.app;
+
+import android.support.v4.app.Fragment;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class IceDetailFragment extends Fragment {
+
+}
---
+++ app/src/main/java/cf/vlado/iceage/app/IceMainFragment.java
@@ -0,0 +1,16 @@
+package cf.vlado.iceage.app;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class IceMainFragment extends Fragment {
+}
